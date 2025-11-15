1d2467470bc24eec013590b2a682e220922d6bc1
Viraj18P <viraj082006@gmail.com>
2025-11-15 23:09:24 +0530

    register/login fixed

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -22,7 +22,10 @@ export default function Register() {
   try {
     const res = await fetch("http://127.0.0.1:8000/register", {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json" 
+        },
       body: JSON.stringify({
         username: formData.username,
         email: formData.email,
